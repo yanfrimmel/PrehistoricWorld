7447eca1385d1eb1694e4dbb7c1c51c88eec9e8a
yanfrimmel <yanfrimmel1411@gmail.com>
2019-02-05 00:12:42 +0200

    Windows dll works

--- src/c/main.c
+++ src/c/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "game.h"
+#ifdef WIN32
+    #include <HsFFI.h>
+#endif
 
 int main( int argc, char** argv ) {
     //HASKELL CALL
+    #ifdef WIN32
+        hs_init(&argc, &argv);  
+    #endif
     int i;
     i = fibonacci_hs(42);
     printf("~~~~~~~~~~~~~~~~~~~~~~~Fibonacci: %d\n", i);
@@ -12,6 +18,9 @@ int main( int argc, char** argv ) {
     renderer = createRenderer();
     printf("Pre gameLoop\n"); 
     play();
+    #ifdef WIN32
+        hs_exit();  
+    #endif
     return 0;
 }
 
--- src/hsbracket.c
+++ src/hsbracket.c
@@ -1,26 +1,53 @@
 /* This file is hsbracket.c. */
-#include <HsFFI.h>
-#include <stdio.h>
+#ifdef WIN32
+#include <windows.h>
 #include <Rts.h>
 
-static void my_enter(void) __attribute__((constructor));
-static void my_enter(void)
-{
-    static char *argv[] = {"", 0};
-    #if defined(__MINGW32__)
-        argv[0] = "libAI.dll";
-    #else
-        argv[0] = "libAI.so";
-    #endif
-    static char **argv_ = argv;
-    static int argc = 1;
-    hs_init(&argc, &argv_);
-    printf("passed hs_init: argv[0]: %s\n", argv[0]);
+// TODO: Make it work for windows!
+
+// extern void __stginit_AI(void);
+
+// BOOL WINAPI DllMain(HINSTANCE hModule, DWORD reason, LPVOID reserved)
+// {
+//   int argc = 1;
+//   static char arg0[] = "libAI.dll";
+//   static char *args[] = {arg0, NULL};
+//   char **argv = args;
+//   hs_init(&argc, &argv);
+//   if (reason == DLL_PROCESS_ATTACH) {
+    
+//     // startupHaskell(1, args, __stginit_AI);
+//     #ifdef __GLASGOW_HASKELL__
+//     hs_add_root(__stginit_AI);
+//     #endif
+//   }
+//   return TRUE;
+// }
+
+
+// static void ai_init() {
+
+// }
+
+// static void ai_exit() {
+// }
+
+#else
+#include <stdlib.h>
+#include <HsFFI.h>
+
+static void ai_init(void) __attribute__((constructor));
+static void ai_init() {
+  int argc = 1;
+  static char arg0[] = "libAI.so";
+  static char *args[] = {arg0, NULL};
+  char **argv = args;
+  hs_init(&argc, &argv);
 }
 
-static void my_exit(void) __attribute__((destructor));
-static void my_exit(void)
-{
-    printf("hs_exit\n");
-    hs_exit();
+static void ai_exit(void) __attribute__((destructor));
+static void ai_exit() {
+  hs_exit();
 }
+
+#endif
