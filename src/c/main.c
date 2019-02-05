61256605c360695d6f35299f6498a0081a355fd3
yanfrimmel <yanfrimmel1411@gmail.com>
2019-02-05 22:37:25 +0200

    Windows and Linux haskell library build mechanism implemeted

--- src/c/main.c
+++ src/c/main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
+#include "HsFFI.h"
 #include "game.h"
-// #ifdef WIN32
-//     #include <HsFFI.h>
-// #endif
 
 int main( int argc, char** argv ) {
+    // start haskell RTS
+    HsStart();  
     //HASKELL CALL
-    // #ifdef WIN32
-    //     hs_init(&argc, &argv);  
-    // #endif
     int i;
     i = fibonacci_hs(42);
     printf("~~~~~~~~~~~~~~~~~~~~~~~Fibonacci: %d\n", i);
@@ -18,9 +15,8 @@ int main( int argc, char** argv ) {
     renderer = createRenderer();
     printf("Pre gameLoop\n"); 
     play();
-    // #ifdef WIN32
-    //     hs_exit();  
-    // #endif
+    // exit haskell RTS
+    HsEnd();  
     return 0;
 }
 
--- src/hsbracket.c
+++ src/hsbracket.c
@@ -1,21 +1,36 @@
-#include <stdlib.h>
-#include "HsFFI.h"
-// This works for GHC 8.0.2
-HsBool mylib_init(void) __attribute__((constructor));
-HsBool mylib_init(void){
-  int argc = 2;
-  char *argv[] = { "+RTS", "-A32m", NULL };
-  char **pargv = argv;
+#ifdef WIN32
+    #include <Rts.h>
+    void HsStart() {
+       int argc = 1;
+       char* argv[] = {"ghcDll", NULL}; // argv must end with NULL
 
-  // Initialize Haskell runtime
-  hs_init(&argc, &pargv);
+       // Initialize Haskell runtime
+       char** args = argv;
+       hs_init(&argc, &args);
+    }
+    void HsEnd() {
+       hs_exit();
+    }
+#else
+    #include <stdlib.h>
+    #include "HsFFI.h"
+        // This works for GHC 8.0.2
+        // HsBool HsStart(void) __attribute__((constructor));
+        HsBool HsStart(void) {
+          int argc = 2;
+          char *argv[] = { "+RTS", "-A32m", NULL };
+          char **pargv = argv;
 
-  // do any other initialization here and
-  // return false if there was a problem
-  return HS_BOOL_TRUE;
-}
+          // Initialize Haskell runtime
+          hs_init(&argc, &pargv);
 
-void mylib_end(void) __attribute__((destructor));
-void mylib_end(void){
-  hs_exit();
-}
+          // do any other initialization here and
+          // return false if there was a problem
+          return HS_BOOL_TRUE;
+        }
+
+        // void HsEnd(void) __attribute__((destructor));
+        void HsEnd(void) {
+          hs_exit();
+        }
+#endif
